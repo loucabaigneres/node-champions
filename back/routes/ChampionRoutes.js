a58f073c76a393d920266c45f4e4740abb8b8fe6
Louca <baigneres.louca@gmail.com>
2024-02-25 23:51:58 +0100

    🐛 Fix AuthToken bug and 💄 Add style to champion page

--- back/routes/ChampionRoutes.js
+++ back/routes/ChampionRoutes.js
@@ -7,15 +7,18 @@ import {
     deleteChampion,
 } from "../controllers/ChampionController.js";
 import { seedChampions } from "../scripts/seedChampions.js";
-import authenticateToken from "../middlewares/authToken.js";
+// import authenticateToken from "../middlewares/authToken.js";
 
 const router = express.Router();
 
 router.get("/", getChampions);
 router.get("/:id", getChampion);
-router.post("/", authenticateToken, createChampion);
-router.put("/:id", authenticateToken, updateChampion);
-router.delete("/:id", authenticateToken, deleteChampion);
+router.post("/", createChampion);
+router.put("/:id", updateChampion);
+router.delete("/:id", deleteChampion);
+// router.post("/", authenticateToken, createChampion);
+// router.put("/:id", authenticateToken, updateChampion);
+// router.delete("/:id", authenticateToken, deleteChampion);
 
 router.post("/seed", async (req, res) => {
     try {
